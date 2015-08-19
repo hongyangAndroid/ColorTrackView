d8d8bfcd4098ab93fce6983c795681e5b4d0059a
hongyangAndroid <lmj623565791@gmail.com>
2015-08-19 10:59:32 +0800

    增加top,bottom

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
 # Android-ColorTrackView
 字体或者图片可以逐渐染色和逐渐褪色的动画效果
 
+# 使用
+
+```xml
+ <com.zhy.view.ColorTrackView
+        android:id="@+id/id_changeTextColorView"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerInParent="true"
+        android:layout_centerVertical="true"
+        android:padding="20dp"
+        android:background="#44ff0000"
+        android:gravity="center_vertical"
+        zhy:progress="0"
+        zhy:direction="left"
+        zhy:text="张鸿洋"
+        zhy:text_change_color="#ffff0000"
+        zhy:text_origin_color="#ff000000"
+        zhy:text_size="60sp" />
+```
+
+注：zhy为命名空间，xmlns:zhy="http://schemas.android.com/apk/res-auto"，可自由修改。
+
+* `progress`  [0.0f , 1.0f]
+* `text` 绘制的文本
+* `text_change_color`目标颜色
+* `text_origin_color`原始颜色
+* `text_size`字体大小
+* `direction `方向，枚举类型，支持：left,right,top,bottom
+
 # 效果图
 
 ## 简单使用
-![Sample Screenshots][1]
+![](sample_ColorTrackeView/sc.gif)
+
 ## 结合ViewPager
-![Sample Screenshots][2]
+![](sample_ColorTrackeView/changecolortvdemo.gif)
+
 
 # 关于我
-[我的博客地址][3]
+[我的博客地址](http://blog.csdn.net/lmj623565791)
 
-[1]: https://github.com/hongyangAndroid/ColorTrackView/blob/master/sample_ColorTrackeView/changecolorTv.gif
-[2]: https://github.com/hongyangAndroid/ColorTrackView/blob/master/sample_ColorTrackeView/changecolortvdemo.gif
-[3]: http://blog.csdn.net/lmj623565791
+
--- library_ColorTrackView/src/com/zhy/view/ColorTrackView.java
+++ library_ColorTrackView/src/com/zhy/view/ColorTrackView.java
@@ -1,41 +1,45 @@
 package com.zhy.view;
 
-import com.zhy.sample_color_change_text.R;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
 import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+
+import com.zhy.sample_color_change_text.R;
+
 /**
  * 
  * @author zhy
- *
+ * 
  */
-public class ColorTrackView extends View
-{
+public class ColorTrackView extends View {
 
 	private int mTextStartX;
-	
-	public enum Direction
-	{
-		LEFT , RIGHT ;
+	private int mTextStartY;
+
+	public enum Direction {
+		LEFT, RIGHT, TOP, BOTTOM;
 	}
 
-	private int mDirection = DIRECTION_LEFT; 
-	
-	private static final int  DIRECTION_LEFT = 0 ; 
-	private static final int  DIRECTION_RIGHT= 1 ;
-	
-	public void setDirection(int direction)
-	{
+	private int mDirection = DIRECTION_LEFT;
+
+	private static final int DIRECTION_LEFT = 0;
+	private static final int DIRECTION_RIGHT = 1;
+	private static final int DIRECTION_TOP = 2;
+	private static final int DIRECTION_BOTTOM = 3;
+
+	public void setDirection(int direction) {
 		mDirection = direction;
 	}
-	
+
 	private String mText = "张鸿洋";
 	private Paint mPaint;
 	private int mTextSize = sp2px(30);
@@ -45,18 +49,15 @@ public class ColorTrackView extends View
 
 	private Rect mTextBound = new Rect();
 	private int mTextWidth;
-
-	private int mRealWidth;
+	private int mTextHeight;
 
 	private float mProgress;
 
-	public ColorTrackView(Context context)
-	{
+	public ColorTrackView(Context context) {
 		super(context, null);
 	}
 
-	public ColorTrackView(Context context, AttributeSet attrs)
-	{
+	public ColorTrackView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -67,60 +68,56 @@ public class ColorTrackView extends View
 		mTextSize = ta.getDimensionPixelSize(
 				R.styleable.ColorTrackView_text_size, mTextSize);
 		mTextOriginColor = ta.getColor(
-				R.styleable.ColorTrackView_text_origin_color,
-				mTextOriginColor);
+				R.styleable.ColorTrackView_text_origin_color, mTextOriginColor);
 		mTextChangeColor = ta.getColor(
-				R.styleable.ColorTrackView_text_change_color,
-				mTextChangeColor);
+				R.styleable.ColorTrackView_text_change_color, mTextChangeColor);
 		mProgress = ta.getFloat(R.styleable.ColorTrackView_progress, 0);
-		
-		mDirection = ta.getInt(R.styleable.ColorTrackView_direction, mDirection);
-		
+
+		mDirection = ta
+				.getInt(R.styleable.ColorTrackView_direction, mDirection);
+
 		ta.recycle();
 
 		mPaint.setTextSize(mTextSize);
-		measureText();
 
 	}
 
 	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-	{
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+		measureText();
+
 		int width = measureWidth(widthMeasureSpec);
 		int height = measureHeight(heightMeasureSpec);
 		setMeasuredDimension(width, height);
 
-		mRealWidth = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
-		mTextStartX = mRealWidth / 2 - mTextWidth / 2;
-
+		mTextStartX = getMeasuredWidth() / 2 - mTextWidth / 2;
+		mTextStartY = getMeasuredHeight() / 2 - mTextHeight / 2;
 	}
 
-	private int measureHeight(int measureSpec)
-	{
+	private int measureHeight(int measureSpec) {
 		int mode = MeasureSpec.getMode(measureSpec);
 		int val = MeasureSpec.getSize(measureSpec);
 		int result = 0;
-		switch (mode)
-		{
+		switch (mode) {
 		case MeasureSpec.EXACTLY:
 			result = val;
 			break;
 		case MeasureSpec.AT_MOST:
 		case MeasureSpec.UNSPECIFIED:
 			result = mTextBound.height();
+			result += getPaddingTop() + getPaddingBottom();
 			break;
 		}
 		result = mode == MeasureSpec.AT_MOST ? Math.min(result, val) : result;
-		return result + getPaddingTop() + getPaddingBottom();
+		return result;
 	}
 
-	private int measureWidth(int measureSpec)
-	{
+	private int measureWidth(int measureSpec) {
 		int mode = MeasureSpec.getMode(measureSpec);
 		int val = MeasureSpec.getSize(measureSpec);
 		int result = 0;
-		switch (mode)
-		{
+		switch (mode) {
 		case MeasureSpec.EXACTLY:
 			result = val;
 			break;
@@ -128,127 +125,200 @@ public class ColorTrackView extends View
 		case MeasureSpec.UNSPECIFIED:
 			// result = mTextBound.width();
 			result = mTextWidth;
+			result += getPaddingLeft() + getPaddingRight();
 			break;
 		}
 		result = mode == MeasureSpec.AT_MOST ? Math.min(result, val) : result;
-		return result + getPaddingLeft() + getPaddingRight();
+		return result;
 	}
 
-	private void measureText()
-	{
+	private void measureText() {
 		mTextWidth = (int) mPaint.measureText(mText);
+		FontMetrics fm = mPaint.getFontMetrics();
+		mTextHeight = (int) Math.ceil(fm.descent - fm.top);
+
 		mPaint.getTextBounds(mText, 0, mText.length(), mTextBound);
+		mTextHeight = mTextBound.height();
 	}
-	
-	public void reverseColor()
-	{
-		int tmp = mTextOriginColor ; 
-		mTextOriginColor = mTextChangeColor ; 
-		mTextChangeColor = tmp ;
+
+	public void reverseColor() {
+		int tmp = mTextOriginColor;
+		mTextOriginColor = mTextChangeColor;
+		mTextChangeColor = tmp;
 	}
 
 	@Override
-	protected void onDraw(Canvas canvas)
-	{
+	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		int r = (int) (mProgress* mTextWidth +mTextStartX );
-		
-		if(mDirection == DIRECTION_LEFT)
-		{
+
+		int r = (int) (mProgress * mTextWidth + mTextStartX);
+		int t = (int) (mProgress * mTextHeight + mTextStartY);
+
+		if (mDirection == DIRECTION_LEFT) {
 			drawChangeLeft(canvas, r);
 			drawOriginLeft(canvas, r);
-		}else
-		{
-			 drawOriginRight(canvas, r);
-			 drawChangeRight(canvas, r);
+		} else if (mDirection == DIRECTION_RIGHT) {
+			drawOriginRight(canvas, r);
+			drawChangeRight(canvas, r);
+		} else if (mDirection == DIRECTION_TOP) {
+			drawOriginTop(canvas, t);
+			drawChangeTop(canvas, t);
+		} else {
+			drawOriginBottom(canvas, t);
+			drawChangeBottom(canvas, t);
 		}
-		
 
 	}
-	
-	private void drawText(Canvas canvas , int color , int startX , int endX)
-	{
+
+	private boolean debug = false;
+
+	private void drawText_h(Canvas canvas, int color, int startX, int endX) {
 		mPaint.setColor(color);
+		if (debug) {
+			mPaint.setStyle(Style.STROKE);
+			canvas.drawRect(startX, 0, endX, getMeasuredHeight(), mPaint);
+		}
 		canvas.save(Canvas.CLIP_SAVE_FLAG);
-		canvas.clipRect(startX, 0, endX, getMeasuredHeight());
-		canvas.drawText(mText, mTextStartX, getMeasuredHeight() / 2
-				+ mTextBound.height() / 2, mPaint);
+		canvas.clipRect(startX, 0, endX, getMeasuredHeight());// left, top,
+																// right, bottom
+		canvas.drawText(mText, mTextStartX,
+				getMeasuredHeight() / 2
+						- ((mPaint.descent() + mPaint.ascent()) / 2), mPaint);
 		canvas.restore();
 	}
-	
-	private void drawChangeRight(Canvas canvas, int r)
-	{
-		drawText(canvas, mTextChangeColor, (int) (mTextStartX +(1-mProgress)*mTextWidth), mTextStartX+mTextWidth );
-	}
-	private void drawOriginRight(Canvas canvas, int r)
-	{
-		drawText(canvas, mTextOriginColor, mTextStartX, (int) (mTextStartX +(1-mProgress)*mTextWidth) );
+
+	private void drawText_v(Canvas canvas, int color, int startY, int endY) {
+		mPaint.setColor(color);
+		if (debug) {
+			mPaint.setStyle(Style.STROKE);
+			canvas.drawRect(0, startY, getMeasuredWidth(), endY, mPaint);
+		}
+
+		canvas.save(Canvas.CLIP_SAVE_FLAG);
+		canvas.clipRect(0, startY, getMeasuredWidth(), endY);// left, top,
+		canvas.drawText(mText, mTextStartX,
+				getMeasuredHeight() / 2
+						- ((mPaint.descent() + mPaint.ascent()) / 2), mPaint);
+		canvas.restore();
 	}
 
-	private void drawChangeLeft(Canvas canvas, int r)
-	{
-		drawText(canvas, mTextChangeColor, mTextStartX, (int) (mTextStartX + mProgress * mTextWidth) );
+	private void drawChangeLeft(Canvas canvas, int r) {
+		drawText_h(canvas, mTextChangeColor, mTextStartX,
+				(int) (mTextStartX + mProgress * mTextWidth));
 	}
 
-	private void drawOriginLeft(Canvas canvas, int r)
-	{
-		drawText(canvas, mTextOriginColor, (int) (mTextStartX + mProgress * mTextWidth), mTextStartX +mTextWidth );
+	private void drawOriginLeft(Canvas canvas, int r) {
+		drawText_h(canvas, mTextOriginColor, (int) (mTextStartX + mProgress
+				* mTextWidth), mTextStartX + mTextWidth);
 	}
 
-	public float getProgress()
-	{
+	private void drawChangeRight(Canvas canvas, int r) {
+		drawText_h(canvas, mTextChangeColor,
+				(int) (mTextStartX + (1 - mProgress) * mTextWidth), mTextStartX
+						+ mTextWidth);
+	}
+
+	private void drawOriginRight(Canvas canvas, int r) {
+		drawText_h(canvas, mTextOriginColor, mTextStartX,
+				(int) (mTextStartX + (1 - mProgress) * mTextWidth));
+	}
+
+	private void drawChangeTop(Canvas canvas, int r) {
+		drawText_v(canvas, mTextChangeColor, mTextStartY,
+				(int) (mTextStartY + mProgress * mTextHeight));
+	}
+
+	private void drawOriginTop(Canvas canvas, int r) {
+		drawText_v(canvas, mTextOriginColor, (int) (mTextStartY + mProgress
+				* mTextHeight), mTextStartY + mTextHeight);
+	}
+
+	private void drawChangeBottom(Canvas canvas, int t) {
+		drawText_v(canvas, mTextChangeColor,
+				(int) (mTextStartY + (1 - mProgress) * mTextHeight),
+				mTextStartY + mTextHeight);
+	}
+
+	private void drawOriginBottom(Canvas canvas, int t) {
+		drawText_v(canvas, mTextOriginColor, mTextStartY,
+				(int) (mTextStartY + (1 - mProgress) * mTextHeight));
+	}
+
+	public float getProgress() {
 		return mProgress;
 	}
 
-	public void setProgress(float progress)
-	{
+	public void setProgress(float progress) {
 		this.mProgress = progress;
 		invalidate();
 	}
 
-	public int getTextSize()
-	{
+	public int getTextSize() {
 		return mTextSize;
 	}
 
-	public void setTextSize(int mTextSize)
-	{
+	public void setTextSize(int mTextSize) {
 		this.mTextSize = mTextSize;
+		mPaint.setTextSize(mTextSize);
+		requestLayout();
+		invalidate();
+	}
+	
+	public void setText(String text) {
+		this.mText = text;
 		requestLayout();
 		invalidate();
 	}
 
-	public int getTextOriginColor()
-	{
+	public int getTextOriginColor() {
 		return mTextOriginColor;
 	}
 
-	public void setTextOriginColor(int mTextOriginColor)
-	{
+	public void setTextOriginColor(int mTextOriginColor) {
 		this.mTextOriginColor = mTextOriginColor;
 		invalidate();
 	}
 
-	public int getTextChangeColor()
-	{
+	public int getTextChangeColor() {
 		return mTextChangeColor;
 	}
 
-	public void setTextChangeColor(int mTextChangeColor)
-	{
+	public void setTextChangeColor(int mTextChangeColor) {
 		this.mTextChangeColor = mTextChangeColor;
 		invalidate();
 	}
 
-	private int dp2px(float dpVal)
-	{
+	private int dp2px(float dpVal) {
 		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
 				dpVal, getResources().getDisplayMetrics());
 	}
 
-	private int sp2px(float dpVal)
-	{
+	private int sp2px(float dpVal) {
 		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
 				dpVal, getResources().getDisplayMetrics());
+	}
+
+	private static final String KEY_STATE_PROGRESS = "key_progress";
+	private static final String KEY_DEFAULT_STATE = "key_default_state";
+
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		Bundle bundle = new Bundle();
+		bundle.putFloat(KEY_STATE_PROGRESS, mProgress);
+		bundle.putParcelable(KEY_DEFAULT_STATE, super.onSaveInstanceState());
+		return bundle;
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+
+		if (state instanceof Bundle) {
+			Bundle bundle = (Bundle) state;
+			mProgress = bundle.getFloat(KEY_STATE_PROGRESS);
+			super.onRestoreInstanceState(bundle
+					.getParcelable(KEY_DEFAULT_STATE));
+			return;
+		}
+		super.onRestoreInstanceState(state);
 	}
 }
